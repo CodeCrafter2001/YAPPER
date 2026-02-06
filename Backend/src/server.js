58259efa6ca955baf3a9fedf017fd1b9df918f1b
CodeCrafter2001 <anmolbansiwal1@gmail.com>
2026-02-06 15:06:45 +0530

    fix: resolve production build issues and static asset serving

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,16 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
-const app = express();
-app.use(express.json());
-const PORT = process.env.PORT || 3000;
 import authRoutes from "./routes/auth.route.js"
 import messageRoute from "./routes/message.route.js"
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 3000;
+
+// 🔥 absolute path — no relative bugs
+const distPath = path.resolve(__dirname, "../../Frontend/dist");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(distPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
+  });
+}
+
+
+
 
 
 app.use('/api/auth', authRoutes); 
 app.use('/api/messages',messageRoute); 
-
 app.listen(PORT,()=>{
     console.log("server is listening on port 3000")
 }); 
- 
+  
