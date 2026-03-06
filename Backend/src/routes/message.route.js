1f2afada3025bb55b2ba3125a0cfc2e385aa64b6
CodeCrafter2001 <anmolbansiwal1@gmail.com>
2026-03-06 08:14:54 +0530

    debugging

--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -1,13 +1,13 @@
 import express from "express";
 const router = express.Router();
-import { getAllContacts } from "../controllers/message.controller.js";
+import { getAllContacts,getMessagesByUserId ,sendMessage} from "../controllers/message.controller.js";
 import { protectRoute } from '../middleware/auth.middleware.js';
 
 
 router.get("/contacts",protectRoute,getAllContacts);
 // router.get("/chats",getChatPartners);
-// router.get("/:id",getMessagesByUserId);
-// router.post("/send/:id",sendMessage);
+router.get("/:id",protectRoute,getMessagesByUserId);
+router.post("/send/:id",protectRoute, sendMessage);
 
 
 
