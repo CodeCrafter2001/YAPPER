7d668aaf2e4dc7b5e8812f7b9403d1b924347eb9
CodeCrafter2001 <anmolbansiwal1@gmail.com>
2026-03-27 16:38:02 +0530

    login-logout added in frontend

--- Frontend/src/pages/ChatPage.jsx
+++ Frontend/src/pages/ChatPage.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import { useAuthStore } from '../store/useAuthStore'
+import React from "react";
+import { useAuthStore } from "../store/useAuthStore";
 
 function ChatPage() {
-  const {logout}= useAuthStore();
-  return (
-    <>
-    <div>
-      ChatPage
-    </div>
+  const { logout } = useAuthStore();
 
-    <div>
-<button onClick={logout}>logout</button>
+  const handleLogout = () => {
+    console.log("logout button clicked");
+    logout();
+  };
+
+  return (
+    <div className="relative z-50 min-h-screen flex items-center justify-center">
+      <button
+        type="button"
+        onClick={handleLogout}
+        className="px-6 py-3 bg-red-500 text-white rounded-lg cursor-pointer"
+      >
+        Logout
+      </button>
     </div>
-    </>
-  )
+  );
 }
 
-export default ChatPage
+export default ChatPage;
